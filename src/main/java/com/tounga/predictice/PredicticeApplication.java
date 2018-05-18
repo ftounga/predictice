463a42969c537d5754e866d41c978128fa104d28
ftounga <ntounga@gmail.com>
2018-05-18 21:23:46 +0200

    first commit

---
+++ src/main/java/com/tounga/predictice/PredicticeApplication.java
@@ -0,0 +1,12 @@
+package com.tounga.predictice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredicticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredicticeApplication.class, args);
+	}
+}
